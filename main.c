2c05c30ce6685c3c1afd559a50824813a670f4c0
andreson <tusharrawat.trf@gmail.com>
2025-11-09 23:57:30 +0800

    Add Ipaddress

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int ipAddress() {
+  printf("IP Address");
+  system("curl ip.me");
+  return 0;
+}
+
+int memInfo() {
   system("head /proc/meminfo");
+  return 0;
+}
+
+int main() {
+  memInfo();
+  ipAddress();
   return 0;
 }
